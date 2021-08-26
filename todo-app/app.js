7290b8fc8021dc7af74a9831f6c890f945fcbcc8
Miguel-Garmor <67270280+Miguel-Garmor@users.noreply.github.com>
2021-08-27 05:38:55 +0900

    Adding todos to list
    The app can now allow users to allow todos to the list. Js creates a <p>, two <button> tags and an <li> element; adds the p tag and buttons to the li element and then adds the li element to the containing Div. After this, the value of the input field is set to an empty string to ready the app for the next task input.

--- todo-app/app.js
+++ todo-app/app.js
@@ -1,7 +1,39 @@
-const selectHeader = document.querySelector('.header');
+//Selectors
+const inputTodo = document.querySelector("#inputTodo");
+const addTodoBtn = document.querySelector("#addTodoBtn");
 
-const header = (e) => {
-     console.log("header");
+const itemList = document.querySelector("#itemList");
+
+//Functions
+const submitTodoHandler = (e) => {
+    e.preventDefault();
+
+    //Create paragraph element
+    const para = document.createElement('p');
+    para.innerText = inputTodo.value;
+    //Create complete button
+    const completeBtn = document.createElement('button');
+    completeBtn.innerText = "Comp";
+    //Create delete button
+    const deleteBtn = document.createElement('button');
+    deleteBtn.innerText = "Del";
+    //Create list element as container
+    const listItem = document.createElement('li');
+    //Add paragraph element to the list
+    listItem.appendChild(para);
+    //Add complete button element to the list
+    listItem.appendChild(completeBtn);
+    //Add delete button element to the list
+    listItem.appendChild(deleteBtn);
+    //Add the list to the containing div
+    itemList.appendChild(listItem);
+
+    //Reset text input value
+    inputTodo.value = '';
+
 };
 
-selectHeader.addEventListener("click", header);
+
+addTodoBtn.addEventListener("click", submitTodoHandler);
+
+
