241c7519e2076ba9b8a09addb14b1e9ca3769db4
Miguel-Garmor <67270280+Miguel-Garmor@users.noreply.github.com>
2021-08-29 14:41:54 +0900

    Created fn checkWin(id);
    This function checks the rows and columns directly that include the last td element clicked on taking in the "id" of the element as a parameter.

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -5,9 +5,47 @@ const table = document.querySelector("#game-area");
 
 //Functions
 
-const checkMark = (e) => {
+const checkWin = (id) => {
+
+    const column = id.slice(1, 2);
+    const row = id.slice(0, 1);
+    let counter = 0;
+
+    //Column check
+    for (i = 0; i < 3; i++) {
+
+        let iToString = i.toString();
+        let colToString = column.toString();
+        let endNum = iToString + colToString;
+        console.log("id"+id);
+    };
+
+    if (counter === 3) {
+        return true;
+    }
+    //-Finish column check
+
+    counter = 0; //Reset counter
+
+    //Row check
+    for (j = 0; j < 3; j++) {
+        let rowToString = row.toString();
+        let jToString = j.toString();
+        let endNum = rowToString + jToString;
+        console.log("id" + id);
+    };
+
+    if (counter === 3) {
+        return true;
+    } else false;
+    //-Finish row check
+
+};
+
+const markHandler = (e) => {
     const targetEl = e.target;
     const table = e.target.parentNode.parentNode;
+    let winCheck = false;
 
     if (targetEl.className === "marked") {
         if (actionCounter === 9) {
@@ -16,12 +54,19 @@ const checkMark = (e) => {
             }
         } else if (actionCounter < 9) {
             console.log("Marked try again");
-            return false;
+            return 0;
         }
     } else {
         targetEl.classList.add("marked");
         targetEl.innerText = 'X';
-        return true;
+
+        //Checks if there is a winner
+        winCheck = checkWin(e.target.id);
+        if (winCheck === false) {
+            return 1;
+        } else return 2;
+        //-End checkWin
+
     }
 
 
@@ -29,13 +74,18 @@ const checkMark = (e) => {
 
 const addMark = (e) => {
 
-    let markChecked = false;
+    let markChecked = 0;
 
-    markChecked = checkMark(e);
+    //Handles X mark
+    markChecked = markHandler(e);
+    //-End markHandler
+    //---checkWin inside
 
-    if (markChecked === true) {
+    if (markChecked === 1) {
         actionCounter++;
         console.log(actionCounter);
+    } else if (markChecked === 2) {
+        console.log("You win!");
     }
 
 
