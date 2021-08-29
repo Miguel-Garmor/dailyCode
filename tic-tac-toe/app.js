23caa626b2dcea92fb6110edc8dce381daab045b
Miguel-Garmor <67270280+Miguel-Garmor@users.noreply.github.com>
2021-08-29 11:21:07 +0900

    Mark box and is box marked functionality
    The app can now mark boxes in the table and tell when a box is marked.
    
    When a box isn't marked and is clicked on the class "marked" is added to it. This way it can tell when a box is marked and only increases the "actionCounter" variable every time a non marked box is clicked.
    
    CSS to fix table cell size has also been added (fixed width and height) so cells keep their size regardless if they have content or not.

---
+++ tic-tac-toe/app.js
@@ -0,0 +1,42 @@
+//Selectors
+const table = document.querySelector("#game-area");
+
+
+
+//Functions
+
+const checkMark = (e) => {
+    const targetEl = e.target;
+    const table = e.target.parentNode.parentNode;
+
+    if (targetEl.className === "marked") {
+        console.log("Marked try again");
+        return false;
+    } else {
+        targetEl.classList.add("marked");
+        targetEl.innerText = 'X';
+        return true;
+    }
+    
+
+};
+
+const addMark = (e) => {
+
+    let markChecked = false;
+
+    markChecked = checkMark(e);
+
+    if(markChecked === true){
+        actionCounter++;
+        console.log(actionCounter);
+    }
+};
+
+
+let actionCounter = 0;
+
+table.addEventListener("click", addMark);
+
+
+
