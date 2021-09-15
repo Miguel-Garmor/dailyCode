5580869903ca593c17a4883725634b999256192d
Miguel-Garmor <67270280+Miguel-Garmor@users.noreply.github.com>
2021-09-15 18:17:30 +0900

    Implemented filterValues()
    The app can now take information from the data.js file and show the relevant information when the user clicks each of the options: daily, weekly or monthly.

--- FrontEndMentor/1.TimeTrackingDashboard/app.js
+++ FrontEndMentor/1.TimeTrackingDashboard/app.js
@@ -1,15 +1,55 @@
 const container = document.querySelector(".container");
-
+const filterContainer = document.querySelector(".filter-container");
 
 
 const setValues = () => {
 
     for (let i = 1; i < container.children.length; i++) {
 
-        container.children[i].children[1].children[0].children[0].innerText = data[i-1].title;
-        console.log(container.children[i].children[1].children[0].children[0]);
+        container.children[i].children[1].children[0].children[0].innerText = data[i - 1].title;
+        console.log(container.children[i].children[1].children[1]);
+    }
+
+}
+
+const filterValues = (e) => {
+    console.log(e.target.innerText);
+
+    switch (e.target.innerText) {
+
+        case "Daily":
+            console.log("This is Daily");
+            for (let i = 1; i < container.children.length; i++) {
+
+                container.children[i].children[1].children[1].innerText = data[i - 1].timeframes.daily.current + "hrs";
+                container.children[i].children[1].children[2].innerText = "Last week - " + data[i - 1].timeframes.daily.previous + "hrs";
+            }
+
+            break;
+        case "Weekly":
+            console.log("This is Weekly");
+            for (let i = 1; i < container.children.length; i++) {
+
+                container.children[i].children[1].children[1].innerText = data[i - 1].timeframes.weekly.current + "hrs";
+                container.children[i].children[1].children[2].innerText = "Last week - " + data[i - 1].timeframes.weekly.previous + "hrs";
+            }
+
+            break;
+        case "Monthly":
+            console.log("This is Monthly");
+            for (let i = 1; i < container.children.length; i++) {
+
+                container.children[i].children[1].children[1].innerText = data[i - 1].timeframes.monthly.current + "hrs";
+                container.children[i].children[1].children[2].innerText = "Last week - " + data[i - 1].timeframes.monthly.previous + "hrs";
+            }
+            break;
+
+        default:
+            break;
     }
 
 }
 
 window.addEventListener("load", setValues);
+
+filterContainer.addEventListener("click", filterValues);
