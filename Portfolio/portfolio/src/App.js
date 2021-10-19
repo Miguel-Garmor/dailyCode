b7f8fae7b02077b929c4fb74cda8ca50ea98b3d5
Miguel-Garmor <67270280+Miguel-Garmor@users.noreply.github.com>
2021-10-20 08:30:01 +0900

    Portfolio site START

---
+++ Portfolio/portfolio/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Contact from './components/Contact';
+import Home from './components/Home';
+import Projects from './components/Projects';
+import Story from './components/Story';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <Projects />
+      <Story />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
