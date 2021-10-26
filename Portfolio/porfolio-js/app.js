ef1f2d7e90aa23f09f8e8ca1dc27b06d5c12b4a0
Miguel-Garmor <67270280+Miguel-Garmor@users.noreply.github.com>
2021-10-26 18:05:27 +0900

    Navigation bar behaviour
    Functions implemented:
     * navBtnLoad(): this function hides the navigation down arrow on document load when the nav bar is visible on the homepage area.
    
     * navBtnHandler(): this function allows toggling of the visibility of the nav bar when the nav button is clicked.
    
     * scrollHandler(): this function handles the behaviour of the nav bar on window scroll.
     - It detects window scroll down and hides the nav bar to free up more space for the content.
    
     - It detects window scroll up and displays the nav bar in case the user wants to navigate to another section faster.
    
     * scrollToLink(): this function allows the user to automatically scroll to a desired section by clicking any of the links in the nav bar
    
    Style and HTML modifications have been added to the form in the Contact section also

--- Portfolio/porfolio-js/app.js
+++ Portfolio/porfolio-js/app.js
@@ -1,10 +1,17 @@
 
 //Global variables
 let startScroll;
+let scrollOnLink;
+let resetScrollOnLink;
 
 //Selectors
 const navBar = document.querySelector("#navigation-bar");
 const navBtn = document.querySelector("#pull-down-btn");
+const links = document.querySelector(".links");
+
+const home = document.querySelector(".home");
+const projects = document.querySelector(".projects");
+const contact = document.querySelector(".contact");
 
 
 //Functions
@@ -20,16 +27,13 @@ const navBtnLoad = () => {
 const navBtnHandler = () => {
     navBar.classList.toggle("hidden");
     navBtn.classList.toggle("hidden");
-
-
 }
 
 const scrollHandler = () => {
-    console.log(window.pageYOffset);
-
     //Nav button handler
     if (window.pageYOffset !== 0) {
         navBtn.classList.remove("hidden");
+        navBtn.style.top = "0";
     } else if (window.pageYOffset === 0) {
         navBtn.classList.add("hidden");
     }
@@ -42,9 +46,9 @@ const scrollHandler = () => {
         navBar.classList.remove("hidden");
 
     }
-
     //Scroll up or down
-    if ((window.pageYOffset - startScroll) <= 0) {
+
+    if ((window.pageYOffset - startScroll) <= 0 && scrollOnLink === false) {
         console.log("scroll up");
         navBar.classList.remove("hidden");
         navBtn.classList.add("hidden");
@@ -54,7 +58,42 @@ const scrollHandler = () => {
         navBtn.classList.remove("hidden");
     }
 
+    //Up scroll limiter
+    if (resetScrollOnLink === true) {
+        scrollOnLink = false;
+    }
     startScroll = window.pageYOffset;
+}
+
+const scrollToLink = (e) => {
+
+    const section = e.target.innerText;
+
+    switch (section) {
+        case "Home":
+            home.scrollIntoView({ behavior: 'smooth', block: 'center' });
+            break;
+        case "Projects":
+            projects.scrollIntoView({ behavior: 'smooth', block: 'center' });
+            navBar.classList.add("hidden");
+            navBtn.classList.remove("hidden");
+            resetScrollOnLink = false;
+            break;
+        case "Contact":
+            contact.scrollIntoView({ behavior: 'smooth', block: 'center' });
+            navBar.classList.add("hidden");
+            navBtn.classList.remove("hidden");
+            break;
+    }
+
+
+    function resetScrollOnLinkHandler() {
+        console.log("resetting scroll link");
+        resetScrollOnLink = true;
+    }
+
+    setTimeout(resetScrollOnLinkHandler, 2000);
+    scrollOnLink = true;
 }
 
 //Event listeners
@@ -64,3 +103,5 @@ window.addEventListener("scroll", scrollHandler);
 window.addEventListener("load", navBtnLoad);
 
 navBtn.addEventListener("click", navBtnHandler);
+
+links.addEventListener("click", scrollToLink);
